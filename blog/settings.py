ececd5ffd20fe8498d89fcac1b40234f48d74cef
Dean Flint <dean.p.flint@gmail.com>
2018-05-04 20:41:13 +0000

    Allowed Hosts updated

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-deanflint.c9users.io', 'blog-test-app-df.heroku.com']
+ALLOWED_HOSTS = ['django-blog-deanflint.c9users.io', 'blog-test-app-df.herokuapp.com']
 
 
 # Application definition
